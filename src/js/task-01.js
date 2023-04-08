1b4009aa191fb4f751af019d40d099356493dca7
tkulagina <tkulagina1974@gmail.com>
2023-04-08 20:55:56 +0300

    Update task-01.js

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,34 +1,14 @@
 const listOfCategories = document.querySelector('#categories');
 console.log ('Number of categories:', listOfCategories.children.length);
 
-/*const dataBaseOfItems = listOfCategories.querySelectorAll('li.item');
-//console.log (dataBaseOfItems);
-
-const namesOfCategories = listOfCategories.querySelector('h2');
-//console.log (namesOfCategories);
-console.log ('Category:', namesOfCategories.textContent);
-
-
-const itemsOfList = namesOfCategories.nextElementSibling;
-//console.log (itemsOfList);
-
-const finalList = itemsOfList.querySelectorAll('li');
-//console.log (finalList);
-console.log ('Elements:', finalList.length);*/
-
-
-
-function getListOfCategoies (listOfCategories) {
-
     const dataBaseOfItems = listOfCategories.querySelectorAll('li.item');
-    console.log (dataBaseOfItems);
+    //console.log (dataBaseOfItems);
 
 
-dataBaseOfItems.forEach (function () {
-    const nameOfList = dataBaseOfItems.querySelector('h2');
-    console.log (nameOfList.textContent);
-    const itemsOfList = namesOfCategories.nextElementSibling;
+dataBaseOfItems.forEach (function (item) {
+    const nameOfList = item.querySelector('h2');
+    console.log ('Category:', nameOfList.textContent);
+    const itemsOfList = nameOfList.nextElementSibling;
     const finalList = itemsOfList.querySelectorAll('li');
     console.log ('Elements:', finalList.length);
 })
-}
